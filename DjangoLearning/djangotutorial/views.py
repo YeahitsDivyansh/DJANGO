6570e797c110444984f056c1a3b6014307c4caae
YeahitsDivyansh <divyansh972003@gmail.com>
2025-06-14 12:49:45 +0530

    Enhance all_djlearning template and views for improved data rendering and styling

--- DjangoLearning/djangotutorial/views.py
+++ DjangoLearning/djangotutorial/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import DjangoVariety
 
 # Create your views here.
 def all_django(request):
-    return render(request, 'djangolearning/all_djlearning.html')
+    all_djangos= DjangoVariety.objects.all()
+    return render(request, 'djangolearning/all_djlearning.html', 
+                  {'all_djangos': all_djangos})
