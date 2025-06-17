446e1550853dadea92796f7046c724012d80c092
YeahitsDivyansh <divyansh972003@gmail.com>
2025-06-17 13:03:57 +0530

    Refactor code structure for improved readability and maintainability

--- DjangoLearning/djangotutorial/admin.py
+++ DjangoLearning/djangotutorial/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import DjangoVariety
+from .models import DjangoVariety, DjangoReview, Store, DjangoCertification
 
 # Register your models here.
-admin.site.register(DjangoVariety)
+class DjangoReviewInline(admin.TabularInline):
+    model = DjangoReview
+    extra = 2
+
+class DjangoVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [DjangoReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('django_varieties',)
+
+class DjangoCertificationAdmin(admin.ModelAdmin):
+    list_display = ('django', 'certification_number', 'date_issued', 'valid_until')
+
+admin.site.register(DjangoVariety, DjangoVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(DjangoCertification, DjangoCertificationAdmin)
--- DjangoLearning/djangotutorial/models.py
+++ DjangoLearning/djangotutorial/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.utils import timezone   
+from django.utils import timezone  
+from django.contrib.auth.models import User
 
 # Create your models here.
-class DjangoVariety(models.Model):
+class DjangoVariety(models.Model): 
     DJANGO_TYPE_CHOICE = [
         ('DJ', 'Django'),
         ('DF', 'Django Rest Framework'),
@@ -22,3 +23,34 @@ class DjangoVariety(models.Model):
 
     def __str__(self):
         return self.name
+    
+# One to Many relationship with User model
+class DjangoReview(models.Model):
+    django = models.ForeignKey(DjangoVariety, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    comment = models.TextField(default='')
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.django.name} Review"
+    
+
+# Many to Many relationship with DjangoVariety
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    django_varieties = models.ManyToManyField(DjangoVariety, related_name='stores')
+   
+    def __str__(self):
+        return self.name
+    
+# One to One relationship with DjangoVariety
+class DjangoCertification(models.Model):
+    django = models.OneToOneField(DjangoVariety, on_delete=models.CASCADE, related_name='certification')
+    certification_number = models.CharField(max_length=100)
+    date_issued = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f"For {self.django.name}"
